9947d1fcc78b2374f5e96f516d2a596832fd399c
Harold Hong <monkeyoohlala@outlook.com>
2025-07-02 15:09:22 -0700

    php code to receive data and htmlspecialchars

--- contact.php
+++ contact.php
@@ -13,7 +13,7 @@
 
     <body>
 
-        <form action="example.html" method="post">
+        <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
             <label for="fullname">Full Name: </label>
             <br>
             <input type="text" id="fullname" name="fullname" placeholder="Full Name" value="">
@@ -24,19 +24,45 @@
             <input type="email" id="email" name="email" placeholder="name@example.com" value="">
             <br><br>
 
-            <label for="topic">Mom: </label>
+            <label for="topic">To Whom You Love The Most: </label>
             <br>
             <input type="text" id="topic" name="topic" placeholder="Dear Mom" value="">
             <br><br>
 
             <label for="message">Write something in here:</label>
             <br>
-            <textarea name="message" id="message" cols="30" rows="10" placeholder="Write a message..."></textarea>
+            <textarea name="message" id="message" cols="30" rows="10" placeholder="I love you... so much"></textarea>
             <br><br>
 
             <button type="submit" value="submit">Send Data</button>
-
         </form>
+
+        <?php 
+            
+
+            if ($_SERVER["REQUEST_METHOD"] == "POST") {
+                $fullname = htmlspecialchars($_POST["fullname"]);
+                $email = htmlspecialchars($_POST["email"]);
+                $topic = htmlspecialchars($_POST["topic"]);
+                $message = htmlspecialchars($_POST["message"]);
+
+
+
+                echo "<br>Thank you, $fullname!<br>" . "We received your email<br>From: $email<br>" . "$topic: " . $message;
+            } else {
+                header("Location: ../contact.php");
+            }
+
+            function validateInput ($data, $fieldName) {
+
+            }
+
+            function validateEmail ($data, $fieldName) {
+                
+            }
+
+
+        ?>
 
     </body>
 
