57cd7c8cfb4747a47cdf74fbb7cb54c7006e9011
Harold Hong <monkeyoohlala@outlook.com>
2025-07-02 16:16:38 -0700

    validation and security

--- contact.php
+++ contact.php
@@ -34,31 +34,76 @@
             <textarea name="message" id="message" cols="30" rows="10" placeholder="I love you... so much"></textarea>
             <br><br>
 
-            <button type="submit" value="submit">Send Data</button>
+            <button type="submit" name="submit" value="submit">Send Data</button>
         </form>
 
         <?php 
+
+
+            if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['submit'])) {
+                $fullname = $email = $topic = $message ='';
+                $fullname_error = $email_error = $topic_error = $message_error = '';
             
+                $originalName = trim($_POST["fullname"]);
+                $originalEmail = trim($_POST["email"]);
+                $originalTopic = trim($_POST["topic"]);
+                $originalMessage = trim($_POST["message"]);
 
-            if ($_SERVER["REQUEST_METHOD"] == "POST") {
-                $fullname = htmlspecialchars($_POST["fullname"]);
-                $email = htmlspecialchars($_POST["email"]);
-                $topic = htmlspecialchars($_POST["topic"]);
-                $message = htmlspecialchars($_POST["message"]);
+                $validation_passed = TRUE;
 
+                //Validate name
+                if (empty($originalName)) {
+                    $name_error = "Please enter your full name. <br>";
+                    echo $name_error;
+                    $validation_passed = FALSE;
+                } else if (strlen($originalName) < 2) {
+                    $name_error = "Name must be at least 2 characters long. <br>";
+                    echo $name_error;
+                    $validation_passed = FALSE;
+                }
 
+                //validate email
+                if (empty($originalEmail)) {
+                    $email_error = "Please enter your email address. <br>";
+                    echo $email_error;
+                    $validation_passed = FALSE;
+                } else if (!filter_var($originalEmail, FILTER_VALIDATE_EMAIL)) {
+                    $email_error = "Please enter a valid email address. <br>";
+                    echo $email_error;
+                    $validation_passed = FALSE;
+                }
 
-                echo "<br>Thank you, $fullname!<br>" . "We received your email<br>From: $email<br>" . "$topic: " . $message;
-            } else {
-                header("Location: ../contact.php");
-            }
+                if (empty($originalTopic)) {
+                    $topic_error = "Please enter a topic. <br>";
+                    echo $topic_error;
+                    $validation_passed = FALSE;
+                }
 
-            function validateInput ($data, $fieldName) {
+                if (empty($originalMessage)) {
+                    $message_error = "Please enter your message. <br>";
+                    echo $message_error;
+                    $validation_passed = FALSE;
+                } 
+                // else if (strlen($originalMessage) < 50) {
+                //     $message_error = "Message length is too short";
+                //     echo $message_error;
+                //     $validation_passed = FALSE;
+                // } else if (strlen($originalMessage) > 150) {
+                //     $message_error = "Message length is too long";
+                //     echo $message_error;
+                //     $validation_passed = FALSE;
+                // }
 
-            }
-
-            function validateEmail ($data, $fieldName) {
+                if ($validation_passed == TRUE) {
+                    
+                    $fullname = htmlspecialchars($originalName, ENT_QUOTES, 'UTF-8');
+                    $email = htmlspecialchars($originalEmail, ENT_QUOTES, 'UTF-8');
+                    $topic = htmlspecialchars($originalTopic, ENT_QUOTES, 'UTF-8');
+                    $message = htmlspecialchars($originalMessage, ENT_QUOTES, 'UTF-8');
                 
+                    echo "<br>Thank you, $fullname!<br>" . "We received your email<br>From: $email<br>" . "$topic, " . $message;
+                }
+
             }
 
 
