9b0c39c0e94faaf9d546f26553254dff9a5b5753
Harold Hong <monkeyoohlala@outlook.com>
2025-07-03 20:27:57 -0700

    near to complete

--- contact.php
+++ contact.php
@@ -1,3 +1,6 @@
+<!-- Harold Hong -->
+<!-- https://github.com/monkeyoohlala/cs85-module3b-createform -->
+
 <!DOCTYPE HTML>
 <html>
     <head>
@@ -6,36 +9,40 @@
         <style>
             textarea:focus {outline: 1px solid blue;}
             input:focus {outline: 1px solid blue;}
-            input[type=text],input[type=email], textarea, select, button{padding: 10px; border-radius: 10px; margin: 5px; border:1px solid black;}
+            input[type=text],input[type=email],input[type=reset],textarea, select, button{padding: 10px; border-radius: 10px; margin: 5px; border:1px solid black;}
             button:hover {background-color: rgb(209,189,164); cursor:pointer;}
+            input[type=reset]:hover {background-color: rgb(209,189,164); cursor:pointer;}
+            .done {text-align: center;}
+            .myForm {display: grid; justify-content: center;}
         </style>
     </head>
 
     <body>
+        <div class="myForm">
+            <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
+                <label for="fullname">Full Name: </label>
+                <br>
+                <input type="text" id="fullname" name="fullname" placeholder="Full Name" value="">
+                <br><br>
 
-        <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
-            <label for="fullname">Full Name: </label>
-            <br>
-            <input type="text" id="fullname" name="fullname" placeholder="Full Name" value="">
-            <br><br>
+                <label for="email">Email: </label>
+                <br>
+                <input type="email" id="email" name="email" placeholder="name@example.com" value="">
+                <br><br>
 
-            <label for="email">Email: </label>
-            <br>
-            <input type="email" id="email" name="email" placeholder="name@example.com" value="">
-            <br><br>
+                <label for="topic">To Whom You Love The Most: </label>
+                <br>
+                <input type="text" id="topic" name="topic" placeholder="Mom" value="">
+                <br><br>
 
-            <label for="topic">To Whom You Love The Most: </label>
-            <br>
-            <input type="text" id="topic" name="topic" placeholder="Dear Mom" value="">
-            <br><br>
-
-            <label for="message">Write something in here:</label>
-            <br>
-            <textarea name="message" id="message" cols="30" rows="10" placeholder="I love you... so much"></textarea>
-            <br><br>
-
-            <button type="submit" name="submit" value="submit">Send Data</button>
-        </form>
+                <label for="message">Write something in here:</label>
+                <br>
+                <textarea name="message" id="message" cols="30" rows="10" placeholder="I love you... so much"></textarea>
+                <br><br>
+                <input type="reset" value="Clear Form" />&nbsp; &nbsp;
+                <button type="submit" name="submit" value="submit">Send Data</button>
+            </form>
+        </div>
 
         <?php 
 
@@ -101,7 +108,9 @@
                     $topic = htmlspecialchars($originalTopic, ENT_QUOTES, 'UTF-8');
                     $message = htmlspecialchars($originalMessage, ENT_QUOTES, 'UTF-8');
                 
-                    echo "<br>Thank you, $fullname!<br>" . "We received your email<br>From: $email<br>" . "$topic, " . $message;
+                    echo "<div class='done'>
+                    <br>Thank you, $fullname!<br>" . "From: $email<br>" . "To $topic, Whom I love the most,<br>" . $message . 
+                    "</div>";
                 }
 
             }
